1f757d573534e05868acfa8240009f59e6ff624d
Arturo Quirós <joseq2408@gmail.com>
2022-09-15 19:34:03 -0600

    initial state

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,34 +1,6 @@
-const cursos = [
-  {
-    titulo: "JavaScript Moderno Guía Definitiva Construye +10 Proyectos",
-    tecnologia: "JavaScript ES6",
-  },
-  {
-    titulo: "React – La Guía Completa: Hooks Context Redux MERN +15 Apps",
-    tecnologia: "React",
-  },
-  {
-    titulo: "Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s",
-    tecnologia: "Node.js",
-  },
-  {
-    titulo: "ReactJS Avanzado – FullStack React GraphQL y Apollo",
-    tecnologia: "React",
-  },
-];
-
 //Resolvers
 const resolvers = {
-  Query: {
-    obtenerCursos: (_, { input }, ctx, info) => {
-      console.log(ctx);
-      const resultados = cursos.filter(
-        (curso) => curso.tecnologia === input.tecnologia
-      );
-      return resultados;
-    },
-    //obtenerTecnologia: () => cursos,
-  },
+  Query: {},
 };
 
 module.exports = resolvers;
--- db/schema.js
+++ db/schema.js
@@ -1,23 +1,6 @@
 const { gql } = require("apollo-server");
 
 //Schema
-const typeDefs = gql`
-  type Curso {
-    titulo: String
-  }
-
-  type Tecnologia {
-    tecnologia: String
-  }
-
-  input CursoInput {
-    tecnologia: String
-  }
-
-  type Query {
-    obtenerCursos(input: CursoInput!): [Curso]
-    obtenerTecnologia: [Tecnologia]
-  }
-`;
+const typeDefs = gql``;
 
 module.exports = typeDefs;

aa7beed856ec5c04b6e80f6a316473859c92be6d
Arturo Quirós <joseq2408@gmail.com>
2022-09-15 20:32:26 -0600

    schemas

--- db/resolvers.js
+++ db/resolvers.js
@@ -3,6 +3,9 @@ const resolvers = {
   Query: {
     obtenerCurso: () => "Hola",
   },
+  Mutation: {
+    nuevoUsuario: () => "Creando nuevo usuario",
+  },
 };
 
 module.exports = resolvers;
--- db/schema.js
+++ db/schema.js
@@ -5,6 +5,18 @@ const typeDefs = gql`
   type Query {
     obtenerCurso: String
   }
+
+  type Usuario {
+    id: ID
+    nombre: String
+    apellido: String
+    email: String
+    creado: String
+  }
+
+  type Mutation {
+    nuevoUsuario: String
+  }
 `;
 
 module.exports = typeDefs;

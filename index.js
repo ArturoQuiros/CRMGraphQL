ee05ae985b8013c372ba4daa8b470ba9c7bb404e
Arturo Quirós <joseq2408@gmail.com>
2022-09-15 19:50:10 -0600

    conexion a mongo

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const { ApolloServer, gql } = require("apollo-server");
 const typeDefs = require("./db/schema");
 const resolvers = require("./db/resolvers");
+const conectarDB = require("./config/db");
+
+//conectar a la BD
+conectarDB();
 
 //server
 const server = new ApolloServer({

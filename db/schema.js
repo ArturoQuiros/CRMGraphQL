67a73aa4a989eac75a3a282cbd279486608f039e
Arturo Quirós <joseq2408@gmail.com>
2022-09-18 14:50:49 -0600

    schema

--- db/schema.js
+++ db/schema.js
@@ -17,9 +17,23 @@ const typeDefs = gql`
     password: String!
   }
 
+  input ProductoInput {
+    nombre: String!
+    existencia: Int!
+    precio: Float!
+  }
+
   input AutenticarInput {
     email: String!
     password: String!
+  }
+
+  type Producto {
+    id: ID
+    nombre: String
+    existencia: Int
+    precio: Float
+    creado: String
   }
 
   type Token {
@@ -27,8 +41,12 @@ const typeDefs = gql`
   }
 
   type Mutation {
+    #Usuarios
     nuevoUsuario(input: UsuarioInput): Usuario
     autenticarUsuario(input: AutenticarInput): Token
+
+    #Productos
+    nuevoProducto(input: ProductoInput): Producto
   }
 
   type Query {

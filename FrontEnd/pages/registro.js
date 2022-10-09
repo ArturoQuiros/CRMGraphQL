77612782db391593d06f135635ea520fb7a27755
Arturo Quirós <joseq2408@gmail.com>
2022-10-09 10:42:43 -0600

    formik

--- FrontEnd/pages/registro.js
+++ FrontEnd/pages/registro.js
@@ -1,7 +1,22 @@
 import React from "react";
 import { Layout } from "../components/Layout";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const registro = () => {
+  const formik = useFormik({
+    initialValues: {
+      nombre: "",
+      apellido: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: (valores) => {
+      console.log("Enviando");
+      console.log(valores);
+    },
+  });
+
   return (
     <div>
       <Layout>
@@ -11,7 +26,10 @@ const registro = () => {
 
         <div className="flex justify-center mt-5">
           <div className="w-full max-w-sm">
-            <form className=" bg-white rounded shadow-md px-8 pt-6 pb-6 mb-4">
+            <form
+              onSubmit={formik.handleSubmit}
+              className=" bg-white rounded shadow-md px-8 pt-6 pb-6 mb-4"
+            >
               <div className="">
                 <label
                   className="block text-gray-700 text-sm font-bold m-2"
@@ -22,8 +40,10 @@ const registro = () => {
                 <input
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="nombre"
-                  type={"text"}
-                  placeholder="nombre"
+                  type="text"
+                  placeholder="Nombre"
+                  value={formik.values.nombre}
+                  onChange={formik.handleChange}
                 ></input>
               </div>
 
@@ -38,7 +58,9 @@ const registro = () => {
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="apellido"
                   type={"text"}
-                  placeholder="apellido"
+                  placeholder="Apellido"
+                  value={formik.values.apellido}
+                  onChange={formik.handleChange}
                 ></input>
               </div>
               <div className="">
@@ -52,7 +74,9 @@ const registro = () => {
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="email"
                   type={"email"}
-                  placeholder="email"
+                  placeholder="Email"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
                 ></input>
               </div>
               <div className="">
@@ -66,7 +90,9 @@ const registro = () => {
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="password"
                   type={"password"}
-                  placeholder="contraseña"
+                  placeholder="Contraseña"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
                 ></input>
               </div>
               <input

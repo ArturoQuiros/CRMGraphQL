1fd94173242deb104961bab3685ff5c885c83652
Arturo Quirós <joseq2408@gmail.com>
2022-10-24 19:10:32 -0600

    login 2

--- FrontEnd/pages/login.js
+++ FrontEnd/pages/login.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
 import { Layout } from "../components/Layout";
-
 import { useFormik } from "formik";
 import * as Yup from "yup";
 
+import { useMutation, gql } from "@apollo/client";
+
+const LOGIN = gql`
+  mutation autenticarUsuario($input: AutenticarInput) {
+    autenticarUsuario(input: $input) {
+      token
+    }
+  }
+`;
+
 const pedidos = () => {
+  //router
+  const router = useRouter();
+  //state para el mensaje
+  const [mensaje, setMensaje] = useState(null);
+  //Mutation
+  const [autenticarUsuario] = useMutation(LOGIN);
+
   const formik = useFormik({
     initialValues: {
       email: "",
@@ -18,13 +35,52 @@ const pedidos = () => {
       password: Yup.string().required("La contraseña es obligatoria"),
     }),
 
-    onSubmit: (valores) => {
-      console.log(valores);
+    onSubmit: async (valores) => {
+      const { email, password } = valores;
+
+      try {
+        const { data } = await autenticarUsuario({
+          variables: {
+            input: {
+              email,
+              password,
+            },
+          },
+        });
+
+        console.log(data);
+        //* Usuario creado correctamente
+        setMensaje("Usuario logeado correctamente!");
+
+        setTimeout(() => {
+          setMensaje(null);
+        }, 3000);
+
+        //* Mandar al index
+        router.push("/");
+      } catch (error) {
+        setMensaje(error.message.replace("GraphQL error: ", ""));
+
+        setTimeout(() => {
+          setMensaje(null);
+        }, 3000);
+      }
     },
   });
+
+  const mostrarMensaje = () => {
+    return (
+      <div className="bg-white py-2 px-3 w-full my-3 max-w-sm text-center mx-auto ">
+        <p>{mensaje}</p>
+      </div>
+    );
+  };
+
   return (
     <div>
       <Layout>
+        {mensaje && mostrarMensaje()}
+
         <h1 className="text-2xl text-white font-light text-center ">Login</h1>
 
         <div className="flex justify-center mt-5">
